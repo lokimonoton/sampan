6e613d4b5db4585d47b6bc68d0cd906a22bda892
polo <slafe29@yandex.com>
2017-03-12 13:52:06 +0000

    bismillah

--- param.js
+++ param.js
@@ -4,9 +4,15 @@ var express=require('express');
 app.get("/", function (req,res) {
 res.send("hai")
 });
- var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
         var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 app.listen(port, ipaddress, function() {
             console.log('%s: Node server started on %s:%d ...',
                         Date(Date.now() ), ipaddress, port);
         });
+        
+app.post("/postmark",bodyParser.json(), function (req,res) {
+    console.log("mulai part 2")
+  console.log(req.body)
+    res.status(200).send("bismillah")
+})
---
+++ postmarkemail.js
@@ -0,0 +1,7 @@
+var postmark=require("./postmark")
+// postmark.buatServer("cenil","http://bismillah-plokot.rhcloud.com/postmark",data=>{
+//     console.log(data)
+// })
+postmark.mendapatkanServer(data=>{
+    console.log(data)
+})
